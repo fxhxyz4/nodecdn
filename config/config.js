b2021f72edb9691dd5282f417cea7249c524ee9f
fxhxyz <fxhxyz@proton.me>
2024-02-26 00:42:35 +0200

    added config.js

---
+++ config/config.js
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+const __dirname = path.dirname(`./`);
+
+const envFiles = ['.env', `.env.${process.env.NODE_ENV}`];
+
+if (process.env.NODE_ENV === 'dev' || process.env.NODE_ENV === 'test') {
+  dotenv.config({
+    path: path.resolve(__dirname, envFiles[1]),
+  });
+} else {
+  dotenv.config({
+    path: path.resolve(__dirname, envFiles[0]),
+  });
+}
+
+const settings = {
+  PORT: process.env.PORT,
+  HOST: process.env.HOST,
+  NODE_ENV: process.env.NODE_ENV,
+};
+
+Object.freeze(settings);
+
+export { settings };
